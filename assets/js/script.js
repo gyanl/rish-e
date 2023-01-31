3796a7696acfd10bb00e84df166eb5d188437caf
Gyan Lakhwani <gyanlakhwani@gmail.com>
2023-02-01 02:03:13 +0530

    pathways

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,16 +2,19 @@ var i = 0;
 var typeBox = document.getElementById("typewriter");
 var txt = typeBox.innerHTML;
 typeBox.innerHTML = "";
-var speed = 30; /* The speed/duration of the effect in milliseconds */
+var speed = 20; /* The speed/duration of the effect in milliseconds */
 
 function typeWriter(){
     if (i < txt.length) {
         typeBox.innerHTML += txt.charAt(i);
         if (txt.charAt(i) == ".") 
         {
-            speed = 240;
-        } else {
-            speed = 30;
+            speed = 1000;
+        } else if (txt.charAt(i) == " ") {
+            speed = 100;
+        }
+        else {
+            speed = 25;
         }
         i++;
         setTimeout(typeWriter, speed);
